1515907bc6e8a6b3ec5e0fd14d456cb1fa707645
Surya Dwipayana <suryadwipayana1001@gmail.com>
2025-12-08 12:05:16 +0800

    soketi npm config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,19 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        host: true,
+        port: 5173,
+        hmr: {
+            host: '192.168.0.155',
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/js/app.jsx'],
             refresh: true,
         }),
-        react(), 
+        react(),
     ],
     resolve: {
         alias: {
